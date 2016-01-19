e710576ac7d1c38f549e299566d3c4dd21c8c114
sanfordcb <sanford0518@gmail.com>
2016-01-18 19:29:17 -0600

    Refactor blinkyDancer and implement, Create wigglyDancer

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,7 +6,7 @@ describe("blinkyDancer", function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it("should have a jQuery $node object", function(){
@@ -23,9 +23,9 @@ describe("blinkyDancer", function() {
     it("should call step at least once per second", function(){
       sinon.spy(blinkyDancer, "step");
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
-
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
