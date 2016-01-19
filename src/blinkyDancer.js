4a71c156f2430b640f2565cff28d60e1d799cbe4
sanfordcb <sanford0518@gmail.com>
2016-01-19 14:07:23 -0600

    Remove comments

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -11,11 +11,3 @@ var BlinkyDancer = function(top, left, timeBetweenSteps){
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
-
-/*BlinkyDancer.prototype.step = function(callback){
-  Dancer.prototype.step.call(this, callback);
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
-};*/
